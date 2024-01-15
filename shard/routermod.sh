dc1e9c962b3c3ba27e4710fcc50c282d64414e54
Preston Davis <pdavis@pebcac.org>
2024-01-15 11:36:48 -0500

    Removed unused files

--- shard/routermod.sh
+++
@@ -1,5 +0,0 @@
-oc patch \
-  -n openshift-ingress-operator \
-  IngressController/default \
-  --type='merge' \
-  -p '{"spec":{"routeSelector":{"matchExpressions":[{"key":"type","operator":"NotIn","values":["sharded"]}]}}}'
--- web2/scc-anyuid.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-oc adm policy add-scc-to-user restricted -z go-web-app-sa -n test
